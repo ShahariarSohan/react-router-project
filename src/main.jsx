a5c9bb9583d05592f8409f94c60af7825abb0cbc
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-09-21 11:31:07 +0600

    This is the first impression of my project

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-7xl">Hello</div>,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
